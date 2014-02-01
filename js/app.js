1a23247b24188d338d60c70529d81937042a4cb9
Abhiroop <asiamgenius@gmail.com>
2014-02-01 15:32:50 +0530

    first commit

---
+++ js/app.js
@@ -0,0 +1,27 @@
+// Configure loading modules from the lib directory,
+// except for 'app' ones, which are in a sibling
+// directory.
+requirejs.config({
+  baseUrl: 'js/lib',
+
+  // Map jquery to zepto
+  map: { '*': { 'jquery': 'zepto' } },
+
+  paths: {
+    app: '../app',
+    models: '../app/models',
+    collections: '../app/collections',
+    views: '../app/views',
+    templates: '../app/templates',
+    db: '../app/db',
+  }
+});
+
+
+requirejs(['views/ViewTodos', 'collections/todos', 'zepto'], function (ViewTodos, todos, $) {
+  new ViewTodos().render();
+
+  //--- for testing only ---//
+  window.todos = todos; //todos collection
+  window.$ = $;         //zepto
+});
